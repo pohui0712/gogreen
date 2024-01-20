6010a1fc6f9622606c39dc96f6f9ec60f70e8288
pohui0712 <phxx04@gmail.com>
2024-01-20 17:38:55 +0800

    add auto slider page

---
+++ web/javascript/autoSlider.js
@@ -0,0 +1,49 @@
+let list = document.querySelector(".slider .slider-list");
+let items = document.querySelectorAll(".slider .slider-list .slider-item");
+let dots = document.querySelectorAll(".dots li");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function () {
+  if (active + 1 > lengthItems) {
+    active = 0;
+  } else {
+    active += 1;
+  }
+  reloadSlider();
+};
+
+prev.onclick = function () {
+  if (active - 1 < 0) {
+    active = lengthItems;
+  } else {
+    active -= 1;
+  }
+};
+
+let refreshSlider = setInterval(() => {
+  next.click();
+}, 5000);
+
+function reloadSlider() {
+  let checkLeft = items[active].offsetLeft;
+  list.style.left = -checkLeft + "px";
+
+  let lastActiveDot = document.querySelector(".dots li.dots-active");
+  lastActiveDot.classList.remove("dots-active");
+  dots[active].classList.add("dots-active");
+  clearInterval(refreshSlider);
+  let refreshSlider = setInterval(() => {
+    next.click();
+  }, 5000);
+}
+
+dots.forEach((li, key) => {
+  li.addEventListener("click", function () {
+    active = key;
+    reloadSlider();
+  });
+});
