4e23c65304904119ffc78525b7c524a9faa056b4
pohui0712 <phxx04@gmail.com>
2024-02-16 18:03:12 +0800

    admin page design

--- web/admin.php
+++ web/admin.php
@@ -14,36 +14,137 @@ include './php/dbConn.php';
 </head>
 <body>
 
-<!-- <div class="conatienr">
-    <form action="./php/upload.php" method="post" enctype="multipart/form-data">
-        <div class="form-group">
-            <label>Select Image File:</label>
-            <input type="file" name="image">
+    <div class="admin-container">
+        <div class="admin-option">
+            <h1>Your Page Title</h1>
+            <h3 id="view-account">View User's Account</h3>
+            <h3 id="view-program">View Programs</h3>
+            <h3 id="add-program">Add Program</h3>
         </div>
-        <div class="form-group">
-            <label>Program Name:</label>
-            <input type="text" name="programName">
-        </div>
-        <div class="form-group">
-            <label>Program Date:</label>
-            <input type="date" name="programDate">
-        </div>
-        <div class="form-group">
-            <label>Program Time:</label>
-            <input type="time" name="programTime">
-        </div>
-        <div class="form-group">
-            <label>Program Location:</label>
-            <input type="text" name="programLocation">
-        </div>
-        <div class="form-group">
-            <label>Program Description:</label>
-            <input type="text" name="programDescription">
-        </div>
-        <input type="submit" name="submit" value="Upload">
-    </form>
-</div> -->
+        <div id="account-table">
+            <h1>Registered User</h1>
+            <table border="1">
+                <tr>
+                    <th>UserID</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Donation</th>
+                </tr>
+                <?php
+                    $accountSQL = "SELECT * FROM testingvolunteers";
+                    $accountResult = $connection->query($accountSQL);
 
+                    if ($accountResult->num_rows > 0) {
+                        while($row = $accountResult->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>" . $row["userID"] . "</td>";
+                            echo "<td>" . $row["username"] . "</td>";
+                            echo "<td>" . $row["email"] . "</td>";
+                            echo "<td>" . $row["donation"] . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='4'>0 results</td></tr>";
+                    } 
+                ?>
+            </table>
+        </div>
+        <div id="program-table">
+            <h1>program</h1>
+            <table border="1">
+                <tr>
+                    <th>Program ID</th>
+                    <th>Name</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Location</th>
+                    <th>Description</th>
+                </tr>
+                <?php
+                    $programSQL = "SELECT * FROM program";
+                    $programResult = $connection->query($programSQL);
+
+                    if ($programResult->num_rows > 0) {
+                        while($row = $programResult->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>" . $row["id"] . "</td>";
+                            echo "<td>" . $row["name"] . "</td>";
+                            echo "<td>" . $row["date"] . "</td>";
+                            echo "<td>" . $row["time"] . "</td>";
+                            echo "<td>" . $row["location"] . "</td>";
+                            echo "<td>" . $row["description"] . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='4'>0 results</td></tr>";
+                    } 
+                ?>
+            </table>
+        </div>
+        </div>
+        <div id="adding-table">
+            <form action="./php/upload.php" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label>Select Image File:</label>
+                    <input type="file" name="image">
+                </div>
+                <div class="form-group">
+                    <label>Program Name:</label>
+                    <input type="text" name="programName">
+                </div>
+                <div class="form-group">
+                    <label>Program Date:</label>
+                    <input type="date" name="programDate">
+                </div>
+                <div class="form-group">
+                    <label>Program Time:</label>
+                    <input type="time" name="programTime">
+                </div>
+                <div class="form-group">
+                    <label>Program Location:</label>
+                    <input type="text" name="programLocation">
+                </div>
+                <div class="form-group">
+                    <label>Program Description:</label>
+                    <input type="text" name="programDescription">
+                </div>
+                <input type="submit" name="submit" value="Upload">
+            </form>
+        </div>
+    </div>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var viewAccount = document.getElementById("view-account");
+            var viewProgram = document.getElementById("view-program");
+            var addProgram = document.getElementById("add-program");
+
+            var accountTable = document.getElementById("account-table");
+            var programTable = document.getElementById("program-table");
+            var addingTable = document.getElementById("adding-table");
+
+            viewAccount.addEventListener("click", function() {
+                accountTable.style.display = "block";
+                programTable.style.display = "none";
+                addingTable.style.display = "none";
+            });
+
+            viewProgram.addEventListener("click", function() {
+                accountTable.style.display = "none";
+                programTable.style.display = "block";
+                addingTable.style.display = "none";
+            });
+
+            addProgram.addEventListener("click", function() {
+                accountTable.style.display = "none";
+                programTable.style.display = "none";
+                addingTable.style.display = "block";
+            });
+        });
+    </script>
+
+</body>
+</html>
 
 </body>
 </html>
